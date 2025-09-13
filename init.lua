e2ddef1349a506a143f01c707ba91dc0c53285a5
Santiago Rodriguez <santo1996.29@proton.me>
2025-09-13 15:35:29 -0400

    Removed treesitter-context plugin

--- init.lua
+++ init.lua
@@ -889,11 +889,6 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-  -- Context from the LSP
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    dependencies = { 'nvim-treesitter/nvim-treesitter' }
-  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
