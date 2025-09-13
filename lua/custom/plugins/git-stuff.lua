22bc92b0a7095e313c28dbafc8465963caeaad5d
Santiago Rodriguez <santo1996.29@proton.me>
2025-09-13 15:39:47 -0400

    Added Neogit plugin

---
+++ lua/custom/plugins/git-stuff.lua
@@ -0,0 +1,17 @@
+-- All things git-related.
+
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    opts = {},
+  },
+}
+
+-- vim: sw=2 ts=2 et
